e527d8f8ed97693919775ac9c59ffb912945d488
DSmith575 <SMITDE5@student.op.ac.nz>
2025-03-20 11:44:05 +1300

    (fix) updated script for build test

--- frontend/src/pages/analytics/Analytics.tsx
+++ frontend/src/pages/analytics/Analytics.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Bar, Line, Pie, Scatter } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 import {
 	Chart as ChartJS,
 	CategoryScale,
