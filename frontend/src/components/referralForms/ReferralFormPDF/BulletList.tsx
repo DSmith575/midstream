71e9e17f43c4d2977c42db1f01b911e60455317a
Ethan McQuarrie <ethanmcquarrie@gmail.com>
2025-08-06 02:12:17 +1200

    Add PDF generation for referral forms

---
+++ frontend/src/components/referralForms/ReferralFormPDF/BulletList.tsx
@@ -0,0 +1,44 @@
+import { Text, View } from '@react-pdf/renderer';
+import type { JSX } from 'react';
+
+const bulletUnicode = (nestingLevel: number): string => {
+    switch(nestingLevel) {
+        case 0:
+            return '\u2022'
+        case 1:
+            return '\u25E6'
+        default:
+            return '\u25AA'
+    }
+}
+
+export interface BulletListItem {
+    value: JSX.Element,
+    nestedList: Array<BulletListItem>,
+}
+
+interface BulletListProps {
+    listItems: Array<BulletListItem>,
+    nestingLevel: number
+}
+
+export const BulletList = ({
+    listItems,
+    nestingLevel = 0
+}: BulletListProps) => {
+    console.log("listItems: ", listItems);
+    return (
+    <View>
+        {listItems.map((item, index) => (
+            <Text key={index} style={{ paddingLeft: (nestingLevel + 1) * 30 }}>
+                {'\t'.repeat(nestingLevel + 1)}
+                {bulletUnicode(nestingLevel)}
+                {' '}
+                {item.value}
+                {'\n'}
+                {<BulletList listItems={item.nestedList} nestingLevel={nestingLevel + 1}/>}
+            </Text>
+        ))}
+    </View>
+    )
+}
---
+++ frontend/src/components/referralForms/ReferralFormPDF/ReferralFormPDF.tsx
@@ -0,0 +1,28 @@
+import { Document, Page, Text, View } from '@react-pdf/renderer';
+import { styles } from './style';
+import { Section } from './Section';
+
+import { generateFormSections } from '@/lib/functions/formFunctions'
+
+interface ReferralFormPDFProps {
+  referralForm: any
+}
+
+export const ReferralFormPDF = ({ referralForm }: ReferralFormPDFProps) => {
+  const formSections = generateFormSections(referralForm, {
+    excludeKeys: ['assignedToWorker'],
+  });
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.title}>
+          <Text>Referral Form</Text>
+        </View>
+          {formSections.map((section, index) => (
+            <Section formSection={section} index={index} />
+          ))}
+      </Page>
+    </Document>
+  )
+}
+
