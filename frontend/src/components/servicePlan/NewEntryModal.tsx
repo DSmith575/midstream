e309e184dd6910c55b852a079402ee6d0a1ff30d
Ethan McQuarrie <ethanmcquarrie@gmail.com>
2025-08-25 03:49:45 +1200

    Use react-forms for service plan entry

--- frontend/src/components/servicePlan/NewEntryModal.tsx
+++ frontend/src/components/servicePlan/NewEntryModal.tsx
@@ -6,14 +6,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import { FormInput } from '../forms/formComponents'
-
-interface ServiceCategory {
-  serviceName: string,
-  flexibleHours: boolean,
-  id: string
-}
+import { ServicePlanEntryForm } from '../forms/servicePlanEntry/ServicePlanEntryForm'
 
 interface NewEntryModalProps {
   servicePlanId: string,
@@ -26,9 +19,6 @@ export const NewEntryModal = ({
   setOpen,
   open,
 }: NewEntryModalProps) => {
-
-  // const { submitServicePlanEntry } = useSubmitServicePlanEntry(servicePlanId);
-  
   return (
     <>
       <Dialog open={open} onOpenChange={() => setOpen(false)}>
@@ -41,36 +31,11 @@ export const NewEntryModal = ({
             </DialogTitle>
           </DialogHeader>
           <DialogDescription>Allocating hours for service</DialogDescription>
-          <fieldset>
-            <label>
-              Category
-            </label>
-            <FormInput />
-          </fieldset>
-          <fieldset>
-            <label>
-              Allocated Hours
-            </label>
-            <input id="hours" />
-          </fieldset>
-          <fieldset>
-            <label>
-              Comment
-            </label>
-            <input id="comment" />
-          </fieldset>
-
-
-          <DialogFooter>
-            <Button
-              variant="outline"
-              size="lg"
-              className="rounded bg-green-500 px-4 py-2 font-bold text-black hover:bg-green-600"
-              // onClick={() => submitServicePlanEntry()}
-            >
-              Add Service Entry
-            </Button>
-          </DialogFooter>
+          <ServicePlanEntryForm
+            servicePlanId={servicePlanId}
+            onSuccess={() => setOpen(false)}
+          />
+          <DialogFooter />
         </DialogContent>
       </Dialog>
     </>
