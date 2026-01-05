8726b1d450a1e1c56674092626f2b8c7acfe991a
Deacon Smith <Deacon.Smith@live.com>
2026-01-06 12:19:31 +1300

    (fix)migrations

--- backend/prisma/migrations/20260105231919_/migration.sql
+++ backend/prisma/migrations/20260105231919_/migration.sql
@@ -1,8 +1,11 @@
 -- CreateEnum
-CREATE TYPE "ReferralStatus" AS ENUM ('SUBMITTED', 'VIEWED', 'ASSIGNED', 'REVIEWED', 'APPROVED', 'REJECTED');
+CREATE TYPE "ReferralStatus" AS ENUM ('CREATED', 'SUBMITTED', 'VIEWED', 'ASSIGNED', 'REVIEWED', 'APPROVED', 'REJECTED');
 
 -- CreateEnum
 CREATE TYPE "UserRole" AS ENUM ('CLIENT', 'WORKER', 'ADMIN', 'MIDSTREAM');
+
+-- CreateEnum
+CREATE TYPE "DocumentType" AS ENUM ('PDF');
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -69,7 +72,11 @@ CREATE TABLE "ReferralForm" (
     "assignedToWorkerId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "status" "ReferralStatus" NOT NULL DEFAULT 'SUBMITTED',
+    "status" "ReferralStatus" NOT NULL DEFAULT 'CREATED',
+    "checklistAudioComplete" BOOLEAN NOT NULL DEFAULT false,
+    "checklistNotesComplete" BOOLEAN NOT NULL DEFAULT false,
+    "checklistReviewComplete" BOOLEAN NOT NULL DEFAULT false,
+    "checklistSubmitComplete" BOOLEAN NOT NULL DEFAULT false,
     "communicationId" TEXT NOT NULL,
     "medicalId" TEXT NOT NULL,
     "disabilityId" TEXT NOT NULL,
@@ -181,11 +188,81 @@ CREATE TABLE "Company" (
     CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "referralId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "transcribedContent" TEXT,
+    "type" "DocumentType" NOT NULL DEFAULT 'PDF',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ReferralNote" (
+    "id" TEXT NOT NULL,
+    "referralId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ReferralNote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ServicePlan" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "serviceCaseId" TEXT NOT NULL,
+
+    CONSTRAINT "ServicePlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ServiceCategory" (
+    "id" TEXT NOT NULL,
+    "serviceName" TEXT NOT NULL,
+    "flexibleHours" BOOLEAN NOT NULL,
+
+    CONSTRAINT "ServiceCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ServicePlanEntry" (
+    "id" TEXT NOT NULL,
+    "servicePlanId" TEXT NOT NULL,
+    "serviceCategoryId" TEXT NOT NULL,
+    "allocatedMinutes" INTEGER NOT NULL,
+
+    CONSTRAINT "ServicePlanEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ServicePlanEntryComment" (
+    "id" TEXT NOT NULL,
+    "servicePlanEntryId" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "ServicePlanEntryComment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ServiceCase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "workerId" TEXT NOT NULL,
+    "referralFormId" TEXT,
+
+    CONSTRAINT "ServiceCase_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_companyId_key" ON "User"("companyId");
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ContactInformation_userId_key" ON "ContactInformation"("userId");
@@ -221,7 +298,13 @@ CREATE UNIQUE INDEX "ReferralForm_consentId_key" ON "ReferralForm"("consentId");
 CREATE UNIQUE INDEX "ReferralForm_additionalInformationId_key" ON "ReferralForm"("additionalInformationId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Company_email_key" ON "Company"("email");
+CREATE UNIQUE INDEX "ServicePlan_serviceCaseId_key" ON "ServicePlan"("serviceCaseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ServiceCategory_serviceName_key" ON "ServiceCategory"("serviceName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ServiceCase_referralFormId_key" ON "ServiceCase"("referralFormId");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -264,3 +347,33 @@ ALTER TABLE "ReferralForm" ADD CONSTRAINT "ReferralForm_additionalInformationId_
 
 -- AddForeignKey
 ALTER TABLE "ReferralForm" ADD CONSTRAINT "ReferralForm_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_referralId_fkey" FOREIGN KEY ("referralId") REFERENCES "ReferralForm"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReferralNote" ADD CONSTRAINT "ReferralNote_referralId_fkey" FOREIGN KEY ("referralId") REFERENCES "ReferralForm"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicePlan" ADD CONSTRAINT "ServicePlan_serviceCaseId_fkey" FOREIGN KEY ("serviceCaseId") REFERENCES "ServiceCase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicePlanEntry" ADD CONSTRAINT "ServicePlanEntry_servicePlanId_fkey" FOREIGN KEY ("servicePlanId") REFERENCES "ServicePlan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicePlanEntry" ADD CONSTRAINT "ServicePlanEntry_serviceCategoryId_fkey" FOREIGN KEY ("serviceCategoryId") REFERENCES "ServiceCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicePlanEntryComment" ADD CONSTRAINT "ServicePlanEntryComment_servicePlanEntryId_fkey" FOREIGN KEY ("servicePlanEntryId") REFERENCES "ServicePlanEntry"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicePlanEntryComment" ADD CONSTRAINT "ServicePlanEntryComment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServiceCase" ADD CONSTRAINT "ServiceCase_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServiceCase" ADD CONSTRAINT "ServiceCase_workerId_fkey" FOREIGN KEY ("workerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServiceCase" ADD CONSTRAINT "ServiceCase_referralFormId_fkey" FOREIGN KEY ("referralFormId") REFERENCES "ReferralForm"("id") ON DELETE SET NULL ON UPDATE CASCADE;
